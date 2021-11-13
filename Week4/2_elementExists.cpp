bed55afc1f1f82faaf08c7b642b61aab6bd7161f
akanksha00 <54508862+akanksha00@users.noreply.github.com>
2021-11-13 21:31:19 +0530

    Update 2_elementExists.cpp

--- Week4/2_elementExists.cpp
+++ Week4/2_elementExists.cpp
@@ -1,14 +1,40 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
     int n,key;
-    bool hash[n];
     cin>>n;
     int a[n];
     for(int i=0;i<n;i++){
         cin>>a[i];
-        hash[a[i]]=true;
     }
+    int m=*max_element(a,a+n);
+    bool hash[m]={false};
+   
+    for(int i=0;i<n;i++){
+       hash[a[i]]=true;
+    }
+     
+    cin>>key;
+    if(hash[key]==true)
+        cout<<"element exists\n";
+    else
+        cout<<"element doen't exists";
+}#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,key;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int m=*max_element(a,a+n);
+    bool hash[m]={false};
+   
+    for(int i=0;i<n;i++){
+       hash[a[i]]=true;
+    }
+     
     cin>>key;
     if(hash[key]==true)
         cout<<"element exists\n";
