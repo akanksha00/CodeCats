7d971e73046a125a6836adf86f7c3d93a9da6576
Akanksha Kumari <54508862+akanksha00@users.noreply.github.com>
2022-01-23 01:19:39 +0530

    Update matrix.cpp

--- Week8/matrix.cpp
+++ Week8/matrix.cpp
@@ -207,4 +207,53 @@
         return true; 
     }
 
-11. 
+11.  vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int, priority_queue<int,vector<int>,greater<int>> > mp;
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                mp[j-i].push(mat[i][j]);
+        
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++){
+                mat[i][j]=mp[j-i].top();
+                mp[j-i].pop();
+            }
+        return mat;
+    }
+
+12. vector<vector<int>> matrixBlockSum(vector<vector<int>>& matrix, int k) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int> > dp(m+1,vector<int>(n+1,0));
+        //build the dp vector
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + matrix[i - 1][j - 1] - dp[i - 1][j - 1];
+            }
+        }
+        
+        
+     
+        vector<vector<int>> ans(m,vector<int>(n,0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                int r1=max(0,i-k)+1; //starting row coordinate of the block
+                int c1=max(0,j-k)+1; //starting column coordinate of the block
+                int r2= min(i+k+1,m); //ending row coordinate of the block
+                int c2= min(j+k+1,n); // ending column coordinate of the block
+                
+                ans[i][j]=dp[r2][c2]+dp[r1-1][c1-1]-dp[r1-1][c2]-dp[r2][c1-1]; //value of the block calculated with the dp vector built
+                
+            }
+        }
+      return ans;  
+        
+        
+    }
